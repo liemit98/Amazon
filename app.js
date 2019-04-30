d62924fcc00277c402124aaa270bf1b98d16fcc9
liemit98 <thaithanhliem2704@gmail.com>
2019-04-30 11:20:35 +0700

    finnis 80%
    liem

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
     var express = require('express');
     var path = require('path');
     var bodyParser = require('body-parser');
+    var session = require('express-session');
     var mysql = require('mysql');
 
     AWS.config.region = process.env.REGION
 
 
 var connection = mysql.createConnection({
-  host     : 'dientoandammaynhom20.clpluyptqi3c.us-east-1.rds.amazonaws.com',
+  host     : 'localhost',
   user     : 'root',
-  password : '123456789',
+  password : '123456',
   port     : 3306,
   database : 'DataNews'
 });
@@ -42,25 +43,94 @@ connection.query("SELECT * FROM news",function(err,result,fields){
     app.use(bodyParser.urlencoded({extended:false}));
     app.use(bodyParser.json())
     app.use(express.static(path.join(__dirname, 'static')));
+    app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }));
 
+    //kiểm tra đăng nhập
+    app.post('/auth', function(request, response) {
+    	var username = request.body.username;
+    	var password = request.body.password;
+    	if (username && password) {
+    		connection.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+    			if (results.length > 0) {
+    				request.session.loggedin = true;
+    				request.session.username = username;
+            request.session.role = results[0].role;
+            if(results[0].role == 0){
+              response.redirect('/');
+            }else {
+              response.redirect('/trangchuadmin');
+            }
+    			} else {
+    				response.send('Incorrect Username and/or Password!');
+    			}
+    			// response.end();
+    		});
+    	} else {
+    		response.send('Please enter Username and Password!');
+    		// response.end();
+    	}
+});
+    //thêm tài khoản...
+    app.post('/adduser', function(request, response) {
+    	var username = request.body.username;
+    	var password = request.body.password;
+      var password1 = request.body.password1;
+      var mail = request.body.mail;
 
+    	if (username && password && mail) {
+        var string = "INSERT INTO `user`(`mail`,`username`,`password`,`role`) VALUES ('"+mail+"','"+username+"','"+password+"',0)";
+    		connection.query(string, function(err, results) {
+          if (err) {
+            console.log(err);
+            return request.status(500).send(err);
+          }
+          response.redirect('/login');
+    		});
+    	} else {
+    		response.send('Please enter Username and Password and Email!');
+    		// response.end();
+    	}
+});
     app.get('/', function(req, res) {
         var mess = [];
+        var kt = 0;
         connection.query("SELECT * FROM news", function (err, result, fields) {
             if (err) throw err;
             //console.log(result);
+            if(req.session.loggedin){
+              kt=1;
               res.render('home', {
                 static_path: 'static',
                 theme: process.env.THEME || 'flatly',
                 flask_debug: process.env.FLASK_DEBUG || 'false',
                 mess : result,
-                type : type
+                type : type,
+                user : req.session.username,
+                kt   : kt
             });
+          }else {
+            res.render('home', {
+              static_path: 'static',
+              theme: process.env.THEME || 'flatly',
+              flask_debug: process.env.FLASK_DEBUG || 'false',
+              mess : result,
+              type : type,
+              kt   : kt
+          });
+          }
         })
     });
 
     app.get('/trangchuadmin', function(req, res) {
               res.render('TrangChuAdmin', {
+                static_path: 'static',
+                theme: process.env.THEME || 'flatly',
+                flask_debug: process.env.FLASK_DEBUG || 'false',
+            });
+    });
+
+    app.get('/login', function(req, res) {
+              res.render('login', {
                 static_path: 'static',
                 theme: process.env.THEME || 'flatly',
                 flask_debug: process.env.FLASK_DEBUG || 'false',
@@ -82,12 +152,16 @@ connection.query("SELECT * FROM news",function(err,result,fields){
         })
     });
 
-    app.get('/khachhang', function(req, res) {
-              res.render('khachhang', {
-                static_path: 'static',
-                theme: process.env.THEME || 'flatly',
-                flask_debug: process.env.FLASK_DEBUG || 'false',
-            });
+    app.get('/admin/khachhang', function(req, res) {
+      connection.query("SELECT * FROM user where role=0", function (err, result, fields) {
+          if (err) throw err;
+            res.render('khachhang', {
+              static_path: 'static',
+              theme: process.env.THEME || 'flatly',
+              flask_debug: process.env.FLASK_DEBUG || 'false',
+              mess : result,
+          });
+      })
     });
 
     app.get('/themtin', function(req, res) {
