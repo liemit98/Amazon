125684deb906fe945f001f98eb569ef69c841b0e
hunghuynh9822 <31771988+hunghuynh9822@users.noreply.github.com>
2019-06-15 06:27:16 +0700

    Revert "b"
    This reverts commit 37303a539b9c033d200eec54056ad4031902c499, reversing
    changes made to 438031e49840250a298edc3d54483decc9f8f1b4.

--- regex.js
+++ regex.js
@@ -1,14 +1,12 @@
 const patternEmail = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9-])+(\.[a-zA-Z0-9]{2,4})+$/;
-const patternUsername = /^([\w\d])+$/;
-const patternInput = /^[^'()?$<>]+$/;
+const patternUsername = /^\w\d+$/;
 const patternPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).{8,10}$/
-const patternContent = /^([\w\s\d\p{L}\p{M}#$^+=!*()@%&-.,;:"])+$/;
+const patternContent = /^([\w\s\d#$^+=!*()@%&-.,;:"])+$/;
 const patternNumber = /^[\d-,.]+$/;
 module.exports = {
     patternEmail,
     patternUsername,
     patternPassword,
     patternContent,
-    patternNumber,
-    patternInput
+    patternNumber
 }
